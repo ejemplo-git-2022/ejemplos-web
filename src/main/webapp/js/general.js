caf3a2e7cc3178381ab1b04f9149ba7f781fc1a3
claudiozam <claudio.zanoszczyk@honou.com.ar>
2022-06-02 15:53:19 -0300

    ejemplo de json con templates

--- src/main/webapp/js/general.js
+++ src/main/webapp/js/general.js
@@ -2,9 +2,22 @@ function calcular() {
 	var campo1 = document.getElementById('campo1');
 	var campo2 = document.getElementById('campo2');
 	var campoTotal = document.getElementById('campoTotal');
-
+	var campoOperacion = document.getElementById('campoOperacion');
+	
+	var valorCampoOperacion = campoOperacion.value;
 	var valorCampo1 = parseFloat(campo1.value);
 	var valorCampo2 = parseFloat(campo2.value);
-
-	campoTotal.value = valorCampo1 + valorCampo2;
+	var total = 0;
+	
+	if(valorCampoOperacion == 'sumar') {
+		total = valorCampo1 + valorCampo2;
+	} else if(valorCampoOperacion == 'restar') {
+		total = valorCampo1 - valorCampo2;
+	} else if(valorCampoOperacion == 'multiplicar') {
+		total = valorCampo1 * valorCampo2;
+	} else if(valorCampoOperacion == 'dividir') {
+		total = valorCampo1 / valorCampo2;
+	}
+	
+	campoTotal.value = total;
 }
